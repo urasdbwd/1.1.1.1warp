cafaacb0ed98a4a798ccd2a3a86a1ee6aa33f5e6
amongus <102772348+urasdbwd@users.noreply.github.com>
2023-09-09 14:22:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 const proxies = JSON.parse(fs.readFileSync('proxies.json', 'utf8'))
-let referrer;
+let referrer = "80804255-d7f5-4efe-823b-eaa4516ae64a"
 function genString(stringLength) {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
@@ -16,65 +16,86 @@ function genString(stringLength) {
   }
   return result;
 }
-
 function makeRequest(proxy) {
-    try {
-      const token = genString(134);
-  
-      const install_id = genString(22);
-      const body = {
-        key: `${genString(43)}=`,
-        install_id: install_id,
-        fcm_token: `${install_id}:APA91b${token}`,
-        referrer: referrer,
-        warp_enabled: false,
-        tos: new Date().toISOString(),
-        type: "Android",
-        locale: "es_ES"
-      };
-  
-      const agent = new HttpsProxyAgent(proxy);
+  return new Promise((resolve, reject) => {
+      try {
+        const token = genString(134);
 
-      fetch("https://api.cloudflareclient.com/v0a591/reg", {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-          'Content-Type': 'application/json; charset=UTF-8',
-          'Host': 'api.cloudflareclient.com',
-          'Connection': 'Keep-Alive',
-          'Accept-Encoding': 'gzip',
-          'User-Agent': 'okhttp/3.12.1'
-        },
-        agent: agent
-      })
-        .then(response => {
+        const install_id = genString(22);
+        const body = {
+          key: `${genString(43)}=`,
+          install_id: install_id,
+          fcm_token: `${install_id}:APA91b${token}`,
+          referrer: referrer,
+          warp_enabled: false,
+          tos: new Date().toISOString(),
+          type: "Android",
+          locale: "es_ES"
+        };
+
+        const agent = new HttpsProxyAgent(proxy);
+
+        fetch("https://api.cloudflareclient.com/v0acd/reg", {
+          method: 'POST',
+          body: JSON.stringify(body),
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Host': 'api.cloudflareclient.com',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip',
+            'User-Agent': 'okhttp/3.12.1'
+          },
+          agent: agent
         })
-        .catch(error => {
-        });
-    } catch (error) {
-    }
-  }
-  
+          .then(response => {
+            if (response.status = 200) {
+              x++
+            }
+          })
+          .catch(error => {
+          });
+      } catch (error) {
+      }
+  });
+}
+let x = 0
 
 async function loopRequest(time) {
   while (true) {
     for (const proxy of proxies) {
       makeRequest(proxy);
-      const delay = 1000 / parseInt(time)
+      let delay = 1000
+      if (time == "inf") {
+        delay = 0
+      } else {
+        delay = 1000 / parseInt(time)
+      }
       await new Promise(resolve => setTimeout(resolve, delay));
     }
   }
 }
+
 let seconds = 0
 let minutes = 0
 let hours = 0
-    referrer = "4f29879e-19ec-4c88-8c18-66e8e889c129"
-    loopRequest(1000)
+rl.question('What is your WARP+ ID?\n', (answer) => {
+  if (!answer) {
+    referrer = "0ab3756c-7732-454f-883a-533ecc15473a"
+  } else {
+    referrer = answer
+  }
+  rl.question('How Many Requests A Second?\n', (a) => {
+    if (!a) {
+      a = 1000
+    }
+    loopRequest(a)
     setInterval(secondcounter, 1000)
     setInterval(minutescounter, 60000)
     setInterval(hourscounter, 3600000)
     setInterval(bored, 250);
-
+    rl.close();
+  });
+});
 
 function secondcounter() {
   seconds++
@@ -107,19 +128,15 @@ const colors = [
     '\x1b[38;5;213m'  // Bright pink foreground
   ];
   const barc = [
-    '\x1b[48;5;223m', // Bright yellow-orange background
-    '\x1b[48;5;229m', // Bright pink-purple background
-    '\x1b[48;5;198m', // Bright lime green background
-    '\x1b[48;5;201m', // Bright pink-red background
-    '\x1b[48;5;221m', // Bright lemon background
-    '\x1b[48;5;227m', // Bright purple background
-    '\x1b[48;5;214m', // Bright peach background
-    '\x1b[48;5;209m', // Bright hot pink background
-    '\x1b[48;5;217m', // Bright yellow background
-    '\x1b[48;5;224m', // Bright yellow-green background
-    '\x1b[48;5;154m', // Bright aqua background
-    '\x1b[48;5;087m', // Bright yellow-green background
-    '\x1b[48;5;213m'  // Bright pink background 
+      '\x1b[41m', // Red background
+      '\x1b[43m', // Yellow background
+      '\x1b[93m', // Bright yellow foreground
+      '\x1b[42m', // Green backgro  rl.close();und
+      '\x1b[92m', // Bright green foreground
+      '\x1b[46m', // Cyan background
+      '\x1b[96m', // Bright cyan foreground
+      '\x1b[45m', // Magenta background
+      '\x1b[95m'  // Bright magenta foreground
   ]
 function getRandomColor() {
   return colors[Math.floor(Math.random() * colors.length)];
@@ -130,12 +147,13 @@ function colorize(text) {
 }
 let colorIndex = 0;
 function bored() {
-  const rainbow = barc[colorIndex] + ' '.repeat(30) + '\x1b[0m';
   console.clear();
   console.log(colorize("Free WARP+ running!"))
+  const rainbow = barc[colorIndex] + ' '.repeat(30) + '\x1b[0m';
   console.log(rainbow.repeat(3));
 console.log(colorize("Running on " + referrer))
 console.log(colorize(`Running for ${hours} hours and ${minutes} minutes and ${seconds} seconds`))
-colorIndex = (colorIndex + 1) % colors.length;
+console.log(`You have gained ${parseInt(x)} gigabytes of data!`)
+  colorIndex = (colorIndex + 1) % barc.length;
 }
 
